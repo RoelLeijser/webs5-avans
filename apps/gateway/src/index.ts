43740a42156f293ea208c73c49480df54098737d
Roel <roelleijser@gmail.com>
2024-02-19 15:47:19 +0100

    Add dotenv

--- apps/gateway/src/index.ts
+++ apps/gateway/src/index.ts
@@ -1,6 +1,6 @@
 import { createServer } from "./server";
 
-const port = process.env.PORT || 3000;
+const port = process.env.GATEWAY_PORT || 3000;
 const server = createServer();
 
 server.listen(port, () => {
